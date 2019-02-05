ab68ae85df195a2307bca3d30da0618d566d31e7
ofgulban <farukgulban@gmail.com>
2019-02-05 23:46:06 +0100

    style changes

--- future/004b_minimal_frame_with_rating_scale.py
+++ future/004b_minimal_frame_with_rating_scale.py
@@ -12,36 +12,23 @@ moni = monitors.Monitor('testMonitor', width=8.2, distance=60)  # in cm
 
 # set screen (make 'fullscr = True' for fullscreen)
 mywin = visual.Window(size=(800, 600), screen=0, winType='pyglet',
-                      allowGUI=True,
-                      fullscr=False,
-                      monitor=moni,
-                      color='grey',
-                      colorSpace='rgb',
-                      units='cm'
-                      )
+                      allowGUI=True, fullscr=False, monitor=moni,
+                      color='grey', colorSpace='rgb', units='cm')
 
 # %%
 """ Stimulus """
 
 stim = visual.GratingStim(win=mywin, tex=None, units='deg',
-                          size=(2, 2), pos=(0, 0),
-                          color='green'
-                          )
+                          size=(2, 2), pos=(0, 0), color='green')
 
 # %%
 """ Response Stimulus """
 
-scale = visual.RatingScale(win=mywin, pos=(0, -0.7),
+scale = visual.RatingScale(win=mywin, pos=(0, -0.7), low=1, high=100,
+                           precision=1, markerStart=50, marker='triangle',
+                           markerColor='DarkRed', tickHeight=0,
                            scale=None,  # text above the slider
-                           low=1, high=100,
-                           precision=1,
-                           markerStart=50,
-                           marker='triangle',
-                           markerColor='DarkRed',
-                           tickHeight=0,
-                           showValue=False,
-                           showAccept=False,
-                           )
+                           showValue=False, showAccept=False)
 
 # %%
 """ Time """
@@ -66,12 +53,12 @@ while clock.getTime() < total_time:
     scale.draw()
     mywin.flip()
 
-print 'Response history (Rating, RT):'
+print('Response history (Rating, RT):')
 pprint.pprint(scale.getHistory())
 history = np.array(scale.getHistory())
 mean, std = np.mean(history[:, 0]), np.std(history[:, 0])
-print 'Average rating: ' + str(mean)
-print 'Standard deviation: ' + str(std)
+print('Average rating: {}'.format(mean))
+print('Standard deviation: {}'.format(std))
 
 mywin.close()
 core.quit()
